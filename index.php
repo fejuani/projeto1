3be988c2121d3f33b021d0ef567b50a0c2303086
Guilherme Closs <guiscloss@gmail.com>
2022-05-12 19:24:11 -0300

    adicionado css

--- index.php
+++ index.php
@@ -6,10 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="artezanato, artes feitas a mão, artes pequenas, pequenas artes"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="CSS/style.css">
     <title>SMALL ART, site para vendedores de artezanatos</title>
 </head>
 <body>
-    
+    <section id="home">
+        <article>
+            <a href="index.php">
+                <ul>
+                    <button class="btn btn-primary" id="pag">Página Inicial</button>
+                </ul>
+            </a>
+            <ul>
+                <button class="btn btn-primary" id="categoria">Categorias</button>
+            </ul>  
+            <ul>
+                <button class="btn btn-primary" id="sobre">Sobre Nós</button>
+            </ul>
+            <ul>
+                <input class="search" id="pesquisa">
+            </ul>
+        </article>
+        
+    </section>
 </body>
 </html>
