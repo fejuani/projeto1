9c9a40b8d31deeb0e72ec0959eeeb599cedae43e
Guilherme Closs <guiscloss@gmail.com>
2022-05-14 00:39:03 -0300

    moldando e atualizando home page

--- index.php
+++ index.php
@@ -1,33 +1,107 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="artezanato, artes feitas a mão, artes pequenas, pequenas artes"> 
+    <meta name="keywords" content="artesanato, artes feitas a mão, artes pequenas, pequenas artes"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/style.css">
-    <title>SMALL ART, site para vendedores de artezanatos</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="shortcut icon" href="img/logo_black.png" type="image/x-icon"/>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>SmallArt</title>
+
 </head>
 <body>
-    <section id="home">
-        <article>
-            <a href="index.php">
-                <ul>
-                    <button class="btn btn-primary" id="pag">Página Inicial</button>
-                </ul>
-            </a>
-            <ul>
-                <button class="btn btn-primary" id="categoria">Categorias</button>
-            </ul>  
-            <ul>
-                <button class="btn btn-primary" id="sobre">Sobre Nós</button>
-            </ul>
-            <ul>
-                <input class="search" id="pesquisa">
-            </ul>
-        </article>
-        
-    </section>
+    <div class="sidebar">
+        <div class="logo_content">
+            <div id="logo">
+                <img src="img/logo.png" alt="SmallArt" class="img_logo">
+                <div >SmallArt</div>
+            </div>
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <ul class="btn">
+            <li>
+                <i class='bx bx-search-alt-2' ></i>
+                <input type="text" placeholder="Pesquisar">
+            </li>       
+            <li>
+                <a href="login.php">
+                    <i class='bx bx-log-in' ></i>
+                    <span class="login">Login</span>
+                </a>
+            </li>
+            <li>
+                <a href="index.php">
+                    <i class='bx bxs-home-smile' ></i>
+                    <span class="home">Página inicial</span>
+                </a>
+            </li>
+            <li>
+                <a href="categoria.html">
+                    <i class='bx bxs-category' ></i>
+                    <span class="Categoria">Categorias</span>
+                </a>
+            </li>
+            <li>
+                <a href="sobre.html">
+                    <i class='bx bxs-group'></i>
+                    <span class="sobre">Sobre Nós</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <ul id="artesanatos">
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+            <li class="img-exemplo">
+            <img class="teste" src="img/img-exemplo.png" alt="Bolsa feita artesanalmente">
+            <p>Bolsa feita artesanalmente</p>
+            <p>R$ 34,00</p>
+            </li>
+        </ul>
+    </div>
+
+    <footer>
+    <p class="copyright">&copy;copyright: SmallArt - 2022</p>
+    </footer>
 </body>
 </html>
